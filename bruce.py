639ead263f6f260223e7ab6a7c23de44466ae4ac
ezaspy <ezaspython@gmail.com>
2020-10-12 20:57:35 +0100

    Utilising tshark for JSON conversion and export

--- bruce.py
+++ bruce.py
@@ -24,22 +24,8 @@ def main():
                     jsonlist, jsondict, objectlist = [], {}, ["dicom", "http", "imf", "smb", "tftp"]
                     if str(pcapcontent.readline())[2:42] == "\\xd4\\xc3\\xb2\\xa1\\x02\\x00\\x04\\x00\\x00\\x00":
                         print("      Processing {}...".format(pcapfile))
-                        pcapcontent = re.sub(r"(\\n)\s*(\d+)", r"\1____________________\2", str(subprocess.Popen(["tshark", "-r", pcapfile, "-t", "ad"], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()))
-                        for packet in pcapcontent.split("\\n____________________"):
-                            eachpkt = str(str(packet).replace("(b'","").replace("\\n', b'')","").replace("\\xe2\\x86\\x92 ","").replace("   "," ").replace("  "," ").strip())
-                            for eachkv in re.findall(r"(?P<PacketID>\d+)\s+(?P<DateTime>\d{4}-\d{2}-\d{2}\s\d+:\d+:\d+\.\d+)\s+(?P<SourceIP>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+(?P<DestinationIP>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+(?P<Protocol>\w+)\s+(?P<PacketSize>\d+)\s+(?P<Payload>[\S\s]+)", eachpkt):
-                                kv = list(eachkv)
-                                if len(kv) > 1:
-                                    jsondict["PacketID"], jsondict["DateTime"], jsondict["SourceIP"], jsondict["DestinationIP"], jsondict["Protocol"], jsondict["PacketSize"], jsondict["Payload"] = kv[0], kv[1], kv[2], kv[3], kv[4], kv[5], kv[6]
-                                else:
-                                    pass
-                                jsonlist.append(json.dumps(jsondict))
-                            jsondict.clear()
                         with open(os.path.join(pcaproot, pcapfile+".json"), "a") as pcapjson:
-                            if len(jsonlist) > 0:
-                                pcapjson.write(str(jsonlist).replace("'{","{").replace("}'","}").replace("'[","[").replace("]'","]").replace("\\\\n","").replace("\\\\\\\\","\\").replace("\\\\\\","\\").replace("\\\\","\\").replace("\\'","'").replace("\\)",")"))
-                            else:
-                                pass
+                            pcapjson.write(str(subprocess.Popen(["tshark", "-r", pcapfile, "-T", "json"], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()).replace("\\\\\\\\","\\").replace("\\\\\\","\\").replace("\\\\r\\\\n","").replace("\\r\\n","").replace("{\\n","{\n").replace("},\\n","},\n").replace("\"\\n","\"\n").replace("\\\\n"," ").replace("\\n","\n").replace("(b'","").replace("', b'')","").replace("\\x","\\\\x"))
                         if export:
                             if not os.path.isdir(os.path.join(pcaproot, str(pcapfile).replace(".pcap","").replace(".gzip",""))):
                                 os.mkdir(os.path.join(pcaproot, str(pcapfile).replace(".pcap","").replace(".gzip","")))
