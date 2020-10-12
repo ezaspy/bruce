60d7b2f1cc5264d0b5e625d0b992849eff949ed3
ezaspy <ezaspython@gmail.com>
2020-10-12 12:16:28 +0100

    Migrated from scapy to tshark

--- bruce.py
+++ bruce.py
@@ -21,40 +21,22 @@ def main():
                 jsonlist, jsondict = [], {}
                 if str(pcapcontent.readline())[2:42] == "\\xd4\\xc3\\xb2\\xa1\\x02\\x00\\x04\\x00\\x00\\x00":
                     print("      Processing {}...".format(pcapfile))
-                    for packet in rdpcap(pcapfile):
-                        pcap_trap = io.StringIO()
-                        sys.stdout = pcap_trap
-                        print(packet.show())
-                        sys.stdout, processed_pcapfile, pcap = sys.__stdout__, str(pcap_trap.getvalue()), {}
-                        pkt = str(re.sub(r"\|\|\|\|\|\|\|\|\|\|\|\|\|\|\|\|\|\|\|\|len(=\d+\|\|\|\|\|\|\|\|\|\|\|\|\|\|\|\|\|\|\|\|)chksum(=0x[A-Fa-f\d]+\|\|\|\|\|\|\|\|\|\|\|\|\|\|\|\|\|\|\|\|)", r"||||||||||||||||||||LEN\1CHKSUM\2", str(re.sub(r"###\[[^\]]+\]###\|\|\|\|\|\|\|\|\|\|\|\|\|\|\|\|\|\|\|\|", r"", str(re.sub(r"src(=\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})", r"srcip\1", str(re.sub(r"dst(=\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})", r"dstip\1", str(re.sub(r"src(=[A-Fa-f\d]{2}\:[A-Fa-f\d]{2}\:[A-Fa-f\d]{2}\:[A-Fa-f\d]{2}\:[A-Fa-f\d]{2}\:[A-Fa-f\d]{2})", r"srcmac\1", str(re.sub(r"dst(=[A-Fa-f\d]{2}\:[A-Fa-f\d]{2}\:[A-Fa-f\d]{2}\:[A-Fa-f\d]{2}\:[A-Fa-f\d]{2}\:[A-Fa-f\d]{2})", r"dstmac\1", str(processed_pcapfile.replace("\\n", "").replace("\n", "|||||||||||||||||||| ").replace("  ", "").replace(" = ","=").replace("= ","=").replace(" =","=").replace(" |||||||||||||||||||| ","||||||||||||||||||||").replace("|||||||||||||||||||| ","||||||||||||||||||||").replace(" ||||||||||||||||||||","||||||||||||||||||||").replace(" ","").replace("'","").replace("\\x","0x").replace("\\","").replace("||||||||||||||||||||load=","||||||||||||||||||||payload_hex="))[0:-7]))))))))))))
-                        if "payload" in pkt:
-                            pkt = pkt+"||||||||||||||||||||payload="+pkt.split("||||||||||||||||||||payload_hex=")[1]
-                        else:
-                            pass
-                        for segment in pkt.split("||||||||||||||||||||"):
-                            if "=" in segment and segment != "None" and len(segment) > 0:
-                                pcap[str(segment.strip().replace("|","").split("=")[0]).replace("|","")] = str(segment.strip().replace("|","").split("=")[1]).replace("0x","")
+                    pcapcontent = re.sub(r"(\\n)\s*(\d+)", r"\1____________________\2", str(subprocess.Popen(["tshark", "-r", pcapfile, "-t", "ad"], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()))
+                    for packet in pcapcontent.split("\\n____________________"):
+                        eachpkt = str(str(packet).replace("(b'","").replace("\\n', b'')","").replace("\\xe2\\x86\\x92 ","").replace("   "," ").replace("  "," ").strip())
+                        for eachkv in re.findall(r"(?P<PacketID>\d+)\s+(?P<DateTime>\d{4}-\d{2}-\d{2}\s\d+:\d+:\d+\.\d+)\s+(?P<SourceIP>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+(?P<DestinationIP>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+(?P<Protocol>\w+)\s+(?P<PacketSize>\d+)\s+(?P<Payload>[\S\s]+)", eachpkt):
+                            kv = list(eachkv)
+                            if len(kv) > 1:
+                                jsondict["PacketID"], jsondict["DateTime"], jsondict["SourceIP"], jsondict["DestinationIP"], jsondict["Protocol"], jsondict["PacketSize"], jsondict["Payload"] = kv[0], kv[1], kv[2], kv[3], kv[4], kv[5], kv[6]
                             else:
                                 pass
-                        for k, v in pcap.items():
-                            if k == "payload":
-                                try: # for i in range(0, len(data), 2) iterates over every second position in data: 0, 2, 4 etc.; data[i:i+2] looks at every pair of hex digits '43', '7c', etc.; chr(int(..., 16))
-                                    v = re.sub(r"[^A-Fa-f\d]", r"", str(''.join(chr(int(v[i:i+2], 16)) for i in range(0, len(v), 2))).replace("\n","").lower())
-                                except:
-                                    pass
-                            else:
-                                pass
-                            if len(v) > 0:
-                                jsondict[k] = v
-                            else:
-                                pass
-                        if len(jsondict) > 0:
                             jsonlist.append(json.dumps(jsondict))
-                            jsondict.clear()
+                        jsondict.clear()
+                    with open(os.path.join(pcaproot, pcapfile+".json"), "a") as pcapjson:
+                        if len(jsonlist) > 0:
+                            pcapjson.write(str(jsonlist).replace("'{","{").replace("}'","}").replace("'[","[").replace("]'","]").replace("\\\\n","").replace("\\\\\\\\","\\").replace("\\\\\\","\\").replace("\\\\","\\").replace("\\'","'").replace("\\)",")"))
                         else:
                             pass
-                    with open(os.path.join(pcaproot, pcapfile+".json"), "a") as pcapjson:
-                        pcapjson.write(str(jsonlist).replace("'{","{").replace("}'","}").replace("'[","[").replace("]'","]").replace("\\\\n","").replace("\\\\","\\"))
                     print("       Done.")
                 else:
                     pass
